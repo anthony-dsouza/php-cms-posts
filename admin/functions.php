cebd7e6af33071cf2f0db5a54323347bc51b27a7
anthony-dsouza <anthonydsouza58@gmail.com>
2021-01-20 23:53:19 +0000

    categories refactoring

---
+++ admin/functions.php
@@ -0,0 +1,114 @@
+<?php
+    function insertCategory() {
+        global $connection;
+        if(isset($_POST['submit'])){
+            $cat_title = $_POST['cat_title'];
+            if($cat_title == "" || empty($cat_title)){
+                echo "This field should not be empty";
+            } else {
+                $query = "INSERT INTO categories(cat_title) VALUE('{$cat_title}')";
+                $add_category = mysqli_query($connection, $query);
+                if(!$add_category) {
+                    die("Query Failed" . mysqli_error($connection));
+                }
+            }
+        }
+    }
+
+    function getAllCategories() {
+        global $connection;
+        $query = "SELECT * FROM categories";
+        $all_categories = mysqli_query($connection, $query);
+        while($row = mysqli_fetch_assoc($all_categories)){
+            $cat_id = $row['cat_id'];
+            $cat_title = $row['cat_title']; 
+?>
+            <tr>
+                <td><?php echo $cat_id; ?></td>
+                <td><?php echo $cat_title; ?></td>
+                <td><a href="categories.php?delete=<?php echo "{$cat_id}"; ?>">Delete</a></td>
+                <td><a href="categories.php?edit=<?php echo "{$cat_id}"; ?>">Edit</a></td>
+            </tr>
+
+ <?php }   
+    }
+
+    function deleteCategory() {
+        global $connection;
+        if(isset($_GET['delete'])) {
+            $get_cat_id = $_GET['delete'];
+            $query = "DELETE FROM categories WHERE cat_id = {$get_cat_id}";
+            $delete_category = mysqli_query($connection, $query);
+            header("Location: categories.php");
+        }  
+    }
+
+    function editCategory() {    
+    ///// edit category                    
+        if(isset($_GET['edit'])) {
+            global $connection;
+            $get_cat_id = $_GET['edit'];
+            $query = "SELECT * FROM categories WHERE cat_id = {$get_cat_id} ";
+            $edit_category = mysqli_query($connection, $query);
+            while($row = mysqli_fetch_assoc($edit_category)){
+                $edit_cat_id = $row['cat_id'];
+                $edit_cat_title = $row['cat_title']; 
+?>
+                <form action="" method="post">
+                <div class="form-group">
+                <label for="cat_title">Edit Category</label>
+                <input value="<?php echo $edit_cat_title; ?>" class="form-control" type="text" name="update_cat_title">
+                </div>
+                <div class="form-group">
+                <input class="btn btn-primary" type="submit" name="update" value="Update">
+                </div>
+                </form>  
+<?php       }    
+        ////// update category
+            if(isset($_POST['update'])) {
+                $update_cat_title = $_POST['update_cat_title'];
+                if($update_cat_title == "" || empty($update_cat_title)){
+                    echo "This field should not be empty";
+                } else {
+                    $query = "UPDATE categories SET cat_title = '{$update_cat_title}' WHERE cat_id = {$edit_cat_id}";
+                    $update_category = mysqli_query($connection, $query);
+                    if(!$update_category) {
+                        die("Query Failed" . mysqli_error($connection));
+                    }
+                    header("Location: categories.php");
+                }
+            }
+        } 
+    }
+
+
+    function getAllPosts() {
+        global $connection;
+        $query = "SELECT * FROM posts";
+        $all_posts = mysqli_query($connection, $query);
+        while($row = mysqli_fetch_assoc($all_posts)){
+            $post_id = $row['post_id'];
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
+            $post_tags = $row['post_tags'];
+?>
+            <tr>
+                <td><?php echo $post_id; ?></td>
+                <td><?php echo $post_title; ?></td>
+                <td><?php echo $post_author; ?></td>
+                <td><?php echo $post_date; ?></td>
+                <td><?php echo $post_image; ?></td>        
+                <td><?php echo $post_content; ?></td>
+                <td><?php echo $post_tags; ?></td>
+<!--
+                <td><a href="posts.php?delete=<?php echo "{$post_id}"; ?>">Delete</a></td>
+                <td><a href="posts.php?edit=<?php echo "{$post_id}"; ?>">Edit</a></td>
+-->
+            </tr>
+
+ <?php }   
+    }
+?>
--- admin/posts.php
+++ admin/posts.php
@@ -17,45 +17,54 @@
                             <small>Author</small>
                         </h1>
                         <!-- Add category form -->
-                        <div class="col-xs-6">
+                        <div class="col-xs-2">
                             <form action="" method="post">
                                 <div class="form-group">
-                                    <label for="cat_title">Add Category</label>
+                                    <label for="cat_title">Add Post</label>
                                     <input class="form-control" type="text" name="cat_title">
                                 </div>
                                 <div class="form-group">
                                     <input class="btn btn-primary" type="submit" name="submit" value="Add">
                                 </div>
                             </form>                                  
+<!--
 <?php
     /////edit and update
     editCategory();
                             
 ?>
+-->
                             <div>
                                 <p>
+<!--
 <?php  
     /////add new category
     insertCategory();
 ?>   
+-->
                                 </p>
                             </div>   
                         </div>
-                        <div class="col-xs-6">
+                        <div class="col-xs-10">
                             <table class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
                                         <th>ID</th>
-                                        <th>Category Title</th>
+                                        <th>Post Title</th>
+                                        <th>Post Author</th>
+                                        <th>Post Date</th>
+                                        <th>Post Image</th>
+                                        <th>Post Content</th>
+                                        <th>Post Tags</th>
                                     </tr>
                                 </thead>
                                 <tbody>
 <?php 
-    ///// category table data 
-    getAllCategories();                    
+    ///// posts table data 
+    getAllPosts();                    
 
     ///// delete category
-    deleteCategory();
+//    deleteCategory();
 ?>                                               
                                 </tbody>
                             </table>
