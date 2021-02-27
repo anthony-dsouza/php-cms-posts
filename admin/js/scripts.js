a9d1d88e90188109fc19ce185ff03be3aa7619d0
anthony-dsouza <anthonydsouza58@gmail.com>
2021-02-27 23:10:45 +0000

    check all

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -10,6 +10,21 @@ $(document).ready(function(){
     
     /// rest of the code
     
+    $('#selectAllBoxes').click(function(event) {
+        
+        if(this.checked) {
+            
+            $('.checkBoxes').each(function() {
+                this.checked = true;
+            });
+        } else {
+            $('.checkBoxes').each(function() {
+                this.checked = false;
+            });
+        }
+        
+        
+    });
     
 });
 
